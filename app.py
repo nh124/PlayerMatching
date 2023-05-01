d43923c44f2f29325c26701ba9c2a5acd524a92a
nh124 <nur.haque99@gmail.com>
2023-05-01 19:19:01 -0400

    App deployment

--- app.py
+++ app.py
@@ -184,8 +184,5 @@ def index():
     return render_template("index.html")
 
 # pylint: disable=invalid-envvar-default
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
-)
+if __name__ == "__main__":
+    app.run(debug=True)
