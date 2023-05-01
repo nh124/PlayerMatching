59167daa1b0e1a2107d1c608dac916f7ece28eae
nh124 <nur.haque99@gmail.com>
2023-05-01 18:57:34 -0400

    App deployment

---
+++ new.py
@@ -0,0 +1,24 @@
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
+
+# Set environment variables for your credentials
+# Read more at http://twil.io/secure
+account_sid = "AC2de301648728adeb00ae67499bd32f53"
+auth_token = "024fbe85057c7ffdab50244d996e1484"
+verify_sid = "VAb03ed0fc17fb6046f36c75ad967c3cd1"
+verified_number = "+14043880607"
+
+client = Client(account_sid, auth_token)
+
+verification = client.verify.v2.services(verify_sid) \
+  .verifications \
+  .create(to=verified_number, channel="sms")
+print(verification.status)
+
+otp_code = input("Please enter the OTP:")
+
+verification_check = client.verify.v2.services(verify_sid) \
+  .verification_checks \
+  .create(to=verified_number, code=otp_code)
+print(verification_check.status)
